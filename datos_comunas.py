7f8307f696a4dec80aab703642021683b0f53849
Francisco-Aros <franciscoarosmunoz@gmail.com>
2022-10-20 15:25:58 -0300

    REsolución desafio 2

--- datos_comunas.py
+++ datos_comunas.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-
+#para leer excel debemos importarlo con la siguiente linea de código. "Header significa que se comienza a leer desde la filaX", Index siginifca que la columa 0 es mi indice
 datos = pd.read_excel("carga-bip.xlsx", header=9, index_col=0)
 print("datos: ", datos)
 
@@ -10,7 +10,7 @@ datos.rename(columns={"MAIPU": "COMUNA"}, inplace=True)
 print("datos: ",datos)
 
 #############################
-#Generar total de datos de puntos de carga por comuna
+##Generar total de datos de puntos de carga por comuna##
 renca= datos[ datos["COMUNA"]=="RENCA" ]
 
 #Obtener Filas y Columnas de los datos.
@@ -36,3 +36,44 @@ datos_tres_comunas = [
     datos[ datos["COMUNA"]=="RENCA" ] ,
     datos[ datos["COMUNA"]=="LA FLORIDA" ]
 ]
+datos_tres_comunas.append(datos[ datos["COMUNA"] == "ÑUÑOA"])
+#Metodo append lo que realiza es agregar un elemento al final de la lista(se pueden usar los dos metodos)
+#Los elementos obtenidos anteriormente, se generan de forma separada, por lo cual se deben unir en un solo elemento
+#Para lo cual primeramente debemos corregir el nombre de la columna correspondiente a la dirección 
+#posteriormente se crea un archivo CSV
+pd.concat(datos_tres_comunas).rename( columns={"CERRO BLANCO 625": "DIRECCION"} ).to_csv("datos-tres-comunas.csv", encoding="UTF-8")
+
+########################################################
+###Generar valores ficticios para horario referencial###
+horario = datos.index
+horarios = []
+for h in horario:
+    # se evalua una condición, en este caso que el CODIGO sea par
+    if (h%2==0):
+        horarios.append("09:00 - 13:00, 14:00 - 19:00")
+    else:
+        horarios.append("08:30 - 12:30, 13:30 - 18:30")
+        
+#Otra forma de escribirlo es:
+# horarios=["09:00 - 13:00, 14:00 - 19:00" if(h % 2 == 0) else "08:30 - 12:30, 13:30 -18:00" for h in horario]
+
+#con la lsita de horarios completa, se asigna a la columna de datos
+datos["HORARIO REFERENCIAL"] = horarios
+print(datos)
+
+################################################################
+###CREAR GRAFICO CON LOS PUNTOS DE CARGA POR CADA HORARIO#######
+#agrupar por cada columna los horarios requeridos
+horario_ref = datos.groupby(["HORARIO REFERENCIAL"])
+#Se crea la referencia al conteo por la columna de horario
+horario_agrupado = horario_ref["HORARIO REFERENCIAL"].count()
+#Se deben cerrar todos los ploteo anteriores
+plt.close("all")
+#Generar el espacio de graficación
+plt.figure()
+#Se debe genera un ploteo de los datos agrupados y contados
+horario_agrupado.plot()
+#Ahora se debe mostrar el gráfico desarrollado
+plt.show()
+#ahora se deben mostrar los datos que se graficaron
+print(horario_agrupado)
